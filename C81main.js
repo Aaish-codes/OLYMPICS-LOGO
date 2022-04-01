8c4eea487740d9c784706412c22f423632fb9071
Aaish-codes <88495440+Aaish-codes@users.noreply.github.com>
2022-04-01 21:15:08 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,51 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "purple";
+ctx.lineWidth= 3;
+ctx.arc(200, 200, 40, 0, 360);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth= 3;
+ctx.arc(250, 200, 40, 0, 360);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth= 3;
+ctx.arc(300, 200, 40, 0, 360);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth= 3;
+ctx.arc(350, 200, 40, 0, 360);
+ctx.stroke();
+
+
+
+
+
+
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth= 3;
+ctx.arc(250, 250, 40, 0, 360);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth= 3;
+ctx.arc(300, 250, 40, 0, 360);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth= 3;
+ctx.arc(350, 250, 40, 0, 360);
+ctx.stroke();
+
